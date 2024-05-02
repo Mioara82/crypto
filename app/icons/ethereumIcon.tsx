0824dd1f52084e7a551f6105a7ef2d432b719272
Mioara82 <128102876+Mioara82@users.noreply.github.com>
2024-05-02 18:49:41 +0100

    created the icon components

--- app/icons/ethereumIcon.tsx
+++ app/icons/ethereumIcon.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
  const EthereumIcon = () => (
     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <g clip-path="url(#clip0_422_7119)">
+    <g clipPath="url(#clip0_422_7119)">
     <circle cx="8" cy="8" r="8" fill="#627EEA"/>
     <path d="M7.9564 2.07959L7.87695 2.34943V10.179L7.9564 10.2583L11.5907 8.10999L7.9564 2.07959Z" fill="#C0CBF6"/>
     <path d="M7.95474 2.07959L4.32031 8.10999L7.95474 10.2583V6.45801V2.07959Z" fill="white"/>
