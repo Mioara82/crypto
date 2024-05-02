85f76a16b14000451b40f2c4f0dd492b9f202727
Mioara82 <128102876+Mioara82@users.noreply.github.com>
2024-05-02 18:56:48 +0100

    added styles to the market data navbar

--- app/components/marketData/marketData.tsx
+++ app/components/marketData/marketData.tsx
@@ -3,13 +3,13 @@ import React, { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../../lib/hooks";
 import { fetchMarketData } from "../../../lib/features/marketSlice";
 import { RootState } from "@/lib/store";
-import { formatMarketCap } from "../../utils/formatHelpers";
 import CoinIcon from "@/app/icons/coinIcon";
 import ExchangeIcon from "@/app/icons/exchangeIcon";
 import CoolIcon from "@/app/icons/coolIcon";
 import BitcoinIcon from "@/app/icons/bitcoinIcon";
 import EthereumIcon from "@/app/icons/ethereumIcon";
 import ProgressBar from "../progressBar";
+ import { formatMarketCap } from "@/app/utils/formatHelpers";
 
 const MarketData = () => {
   const dispatch = useAppDispatch();
@@ -27,52 +27,68 @@ const MarketData = () => {
     return <div>Loading data...</div>;
   }
 
-  const coinData = marketData.data.active_cryptocurrencies;
-  const btcMarketCapPercentage =
-    marketData.data.market_cap_percentage.btc.toFixed(0);
-  const ethMarketCapPercentage =
-    marketData.data.market_cap_percentage.eth.toFixed(0);
-  const totalMarketCap = formatMarketCap(marketData.data.total_market_cap.usd);
-  const totalVolume = formatMarketCap(marketData.data.total_volume.usd);
-  const exchange = marketData.data.markets;
+  const getTotalMarketVolume = () =>{
+    if(marketData && marketData.totalVolume){
+      const totalMarketVolume = Object.values(marketData.totalVolume).reduce(
+      (a: any, b: any) => a + b,
+      0
+    );
+  return totalMarketVolume;
+    }
+};
+  const currencyVolumeRatio = (
+    (marketData?.totalVolumePerCurrency / getTotalMarketVolume()) *
+    100
+  ).toFixed(3);
 
   return (
-    <nav className="flex justify-center my-2 m-0 bg-light-darkBg py-4 rounded-t-md max-w-1440">
-      {loadingStatus === "fulfilled" && (
-        <div className="inline-flex gap-2 text-xs ">
-          <div className="inline-flex justify-items-center gap-1 text-light-lightTextColor">
-            <CoinIcon />
-            Coins
+    marketData && (
+      <nav className="flex justify-around my-2 m-0 flex-nowrap bg-light-darkBg py-4 rounded-t-md max-w-1440">
+        {loadingStatus === "fulfilled" && (
+          <div className="flex gap-2 text-xs ">
+            <div className="inline-flex justify-items-center gap-1 text-light-lightTextColor">
+              <CoinIcon />
+              Coins
+            </div>
+            <div className="text-light-primary">{marketData.coinData}</div>
+            <div className="inline-flex justify-items-center gap-1 text-light-lightTextColor">
+              <ExchangeIcon />
+              Exchange
+            </div>
+            <div className="text-light-primary">{marketData.exchange}</div>
+            <div className="inline-flex justify-items-center gap-1 text-light-lightTextColor">
+              <CoolIcon />
+              <div className="text-light-primary">
+                {marketData.totalMarketCap}
+              </div>
+            </div>
+            <div>
+              <div className="text-light-primary">${formatMarketCap(marketData.totalVolumePerCurrency)}</div>
+              <ProgressBar value={currencyVolumeRatio} />
+            </div>
+            <div className="inline-flex justify-items-center items-center gap-1 text-light-lightTextColor">
+              <BitcoinIcon />
+              <div>{marketData.btcMarketCapPercentage}%</div>
+              <ProgressBar
+                value={marketData.btcMarketCapPercentage}
+                color="orange"
+              />
+            </div>
+            <div className="inline-flex justify-items-center items-center gap-1 text-light-lightTextColor">
+              <EthereumIcon />
+              <div>{marketData.ethMarketCapPercentage}%</div>
+              <ProgressBar
+                value={marketData.ethMarketCapPercentage}
+                color="purple"
+              />
+            </div>
           </div>
-          <div className="text-light-primary">{coinData}</div>
-          <div className="inline-flex justify-items-center gap-1 text-light-lightTextColor">
-            <ExchangeIcon />
-            Exchange
-          </div>
-          <div className="text-light-primary">{exchange}</div>
-          <div className="inline-flex justify-items-center gap-1 text-light-lightTextColor">
-            <CoolIcon />
-            <div className="text-light-primary">{totalMarketCap}</div>
-          </div>
-          <div>
-            <div className="text-light-primary">${totalVolume}</div>
-            <ProgressBar value={totalVolume} />
-          </div>
-          <div className="inline-flex justify-items-center items-center gap-1 text-light-lightTextColor">
-            <BitcoinIcon />
-            <div>{btcMarketCapPercentage}%</div>
-            <ProgressBar value={btcMarketCapPercentage} orange />
-          </div>
-          <div className="inline-flex justify-items-center items-center gap-1 text-light-lightTextColor">
-            <EthereumIcon />
-            <div>{ethMarketCapPercentage}%</div>
-            <ProgressBar value={ethMarketCapPercentage} purple />
-          </div>
-        </div>
-      )}
-      {loadingStatus === "rejected" && <p>Error: {error}</p>}
-    </nav>
+        )}
+        {loadingStatus === "rejected" && <p>Error: {error}</p>}
+      </nav>
+    )
   );
+  
 };
 
 export default MarketData;
