9aad0f9dca21d141c1f4b737e8457454a52d6a67
Mioara82 <128102876+Mioara82@users.noreply.github.com>
2024-05-04 00:45:31 +0100

    rendered the marketData inside the navbar component

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,18 @@
+"use client";
+
 import ThemeSwitch from "./ThemeSwitch";
+import StoreProvider from "../providers/StoreProvider";
+import MarketData from "./marketData/marketData";
 
 const Navbar = () => {
-    return <ThemeSwitch/>;
+  return (
+    <>
+      <StoreProvider>
+        <MarketData />
+      </StoreProvider>
+      <ThemeSwitch />
+    </>
+  );
 };
 
 export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
 "use client";
 
-import StoreProvider from "./providers/StoreProvider";
-import MarketData from "./components/marketData/marketData";
-
 export default function Home() {
-  return (
-    <StoreProvider>
-      <MarketData />
-    </StoreProvider>
-  );
+  return <h4>Home</h4>;
 }
