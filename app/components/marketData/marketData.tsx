339536cc38377776a420abc25ffcd2553e8ca41e
Mioara82 <128102876+Mioara82@users.noreply.github.com>
2024-05-04 02:20:57 +0100

    refactored the progress bar component

--- app/components/marketData/marketData.tsx
+++ app/components/marketData/marketData.tsx
@@ -9,7 +9,7 @@ import ArrowIcon from "@/app/icons/arrowIcon";
 import BitcoinIcon from "@/app/icons/bitcoinIcon";
 import EthereumIcon from "@/app/icons/ethereumIcon";
 import ProgressBar from "../progressBar";
- import { formatMarketCap } from "@/app/utils/formatHelpers";
+import { formatMarketCap } from "@/app/utils/formatHelpers";
 
 const MarketData = () => {
   const dispatch = useAppDispatch();
@@ -27,15 +27,15 @@ const MarketData = () => {
     return <div>Loading data...</div>;
   }
 
-  const getTotalMarketVolume = () =>{
-    if(marketData && marketData?.totalVolume){
+  const getTotalMarketVolume = () => {
+    if (marketData && marketData?.totalVolume) {
       const totalMarketVolume = Object.values(marketData?.totalVolume).reduce(
-      (a, b) => a + b,
-      0
-    );
-  return totalMarketVolume;
+        (a, b) => a + b,
+        0
+      );
+      return totalMarketVolume;
     }
-};
+  };
   const currencyVolumeRatio = (
     (marketData?.totalVolumePerCurrency / getTotalMarketVolume()) *
     100
@@ -45,41 +45,52 @@ const MarketData = () => {
     marketData && (
       <nav className="flex justify-around my-2 m-0 flex-nowrap bg-light-darkBg py-4 rounded-t-md max-w-1440">
         {loadingStatus === "fulfilled" && (
-          <div className="flex gap-2 text-xs ">
-            <div className="inline-flex justify-items-center gap-1 text-light-lightTextColor">
-              <CoinIcon />
-              Coins
+          <div className="flex gap-8 text-xs ">
+            <div className="inline-flex justify-items-center gap-2">
+              <div className="inline-flex justify-items-center gap-1 text-light-lightTextColor">
+                <CoinIcon />
+                Coins
+              </div>
+              <div className="text-light-primary">{marketData.coinData}</div>
             </div>
-            <div className="text-light-primary">{marketData.coinData}</div>
-            <div className="inline-flex justify-items-center gap-1 text-light-lightTextColor">
-              <ExchangeIcon />
-              Exchange
+
+            <div className="inline-flex justify-items-center gap-2">
+              <div className="inline-flex justify-items-center gap-1 text-light-lightTextColor">
+                <ExchangeIcon />
+                Exchange
+              </div>
+              <div className="text-light-primary">{marketData.exchange}</div>
             </div>
-            <div className="text-light-primary">{marketData.exchange}</div>
+
             <div className="inline-flex justify-items-center gap-1 text-light-lightTextColor">
               <ArrowIcon />
               <div className="text-light-primary">
                 {marketData.totalMarketCap}
               </div>
             </div>
-            <div>
-              <div className="text-light-primary">${formatMarketCap(marketData.totalVolumePerCurrency)}</div>
-              <ProgressBar value={currencyVolumeRatio} />
+
+            <div className="inline-flex justify-items-center items-center gap-[5px]">
+              <div className="text-light-primary">
+                ${formatMarketCap(marketData.totalVolumePerCurrency)}
+              </div>
+              <ProgressBar value={currencyVolumeRatio} color="#ffffff" />
             </div>
-            <div className="inline-flex justify-items-center items-center gap-1 text-light-lightTextColor">
+
+            <div className="inline-flex justify-items-center items-center gap-[5px] text-light-lightTextColor">
               <BitcoinIcon />
               <div>{marketData.btcMarketCapPercentage}%</div>
               <ProgressBar
                 value={marketData.btcMarketCapPercentage}
-                color="orange"
+                color="#F7931A"
               />
             </div>
-            <div className="inline-flex justify-items-center items-center gap-1 text-light-lightTextColor">
+
+            <div className="inline-flex justify-items-center items-center gap-[5px] text-light-lightTextColor">
               <EthereumIcon />
               <div>{marketData.ethMarketCapPercentage}%</div>
               <ProgressBar
                 value={marketData.ethMarketCapPercentage}
-                color="purple"
+                color="#849DFF"
               />
             </div>
           </div>
@@ -88,7 +99,6 @@ const MarketData = () => {
       </nav>
     )
   );
-  
 };
 
 export default MarketData;
--- app/components/progressBar.tsx
+++ app/components/progressBar.tsx
@@ -1,10 +1,33 @@
-const ProgressBar = (props:any) => {
-    return(
-        <progress className="w-[53px] h-[6px] sm:rounded-sm"
-            max="100"
-            value={props.value}
+import { Progress } from "semantic-ui-react";
+import styled from "styled-components";
+
+const StyledProgressBar = styled(Progress)`
+  &&& {
+    .bar {
+      background-color: ${(props) => props.color || "green"} !important;
+      height: 6px;
+      border-radius: 2px;
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+  }
+`;
+
+const ProgressBar = (props: any) => {
+  return (
+    <>
+      <div className="relative w-[53px] h-[6px] rounded-sm bg-[#FFFFFF66]">
+        <StyledProgressBar
+          percent={props.value}
+          total={100}
+          style={{ width: "53px", margin: 0 }}
+          color={props.color}
+          size="tiny"
         />
-    );
+      </div>
+    </>
+  );
 };
 
 export default ProgressBar;
