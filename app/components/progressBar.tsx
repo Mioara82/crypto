271de227b2da8889eb2557f6d71070b027e26297
Mioara82 <128102876+Mioara82@users.noreply.github.com>
2024-05-05 23:37:41 +0100

    wip fixed eslint error

--- app/components/progressBar.tsx
+++ app/components/progressBar.tsx
@@ -1,32 +1,15 @@
-import { Progress } from "semantic-ui-react";
-import styled from "styled-components";
-
-const StyledProgressBar = styled(Progress)`
-  &&& {
-    .bar {
-      background-color: ${(props) => props.color || "green"} !important;
-      height: 6px;
-      border-radius: 2px;
-      position: absolute;
-      top: 0;
-      left: 0;
-    }
-  }
-`;
+import React from "react";
 
 const ProgressBar = (props: any) => {
+  const barWidth = Math.round(0.53 * props.value);
+  const barColor = props.color;
   return (
-    <>
-      <div className="relative w-[53px] h-[6px] rounded-sm bg-[#FFFFFF66]">
-        <StyledProgressBar
-          percent={props.value}
-          total={100}
-          style={{ width: "53px", margin: 0 }}
-          color={props.color}
-          size="tiny"
-        />
-      </div>
-    </>
+    <div className="relative w-[53px] h-[6px] rounded-sm bg-[#FFFFFF66]">
+      <div
+        className="absolute top-0 left-0 bg-common-purple"
+        style={{ width: `${barWidth}px`, background: barColor }}
+      ></div>
+    </div>
   );
 };
 
