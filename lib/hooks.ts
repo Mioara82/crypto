348b8106410d724cb5611abdac7fe68d422b3750
Mioara82 <128102876+Mioara82@users.noreply.github.com>
2024-05-11 00:58:29 +0100

    fixed indentation errors

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,6 +1,19 @@
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector, useStore } from "react-redux";
 import type { TypedUseSelectorHook } from "react-redux";
 import type { RootState, AppDispatch, AppStore } from "./store";
+
+export function useLocalState(key: string, initialValue: string) {
+  const storedValue = window.localStorage.getItem(key);
+  const item = storedValue ? JSON.parse(storedValue) : initialValue;
+  const [value, setValue] = useState(item);
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [value]);
+
+  return [value, setValue];
+}
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
