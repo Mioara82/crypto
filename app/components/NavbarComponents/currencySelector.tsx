d14bb2889b0aa706833a462c057702be1886a3c2
Mioara82 <128102876+Mioara82@users.noreply.github.com>
2024-05-17 01:58:46 +0100

    imported frame motion and created motion elements

--- app/components/NavbarComponents/currencySelector.tsx
+++ app/components/NavbarComponents/currencySelector.tsx
@@ -1,28 +1,59 @@
 "use-client";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
 import { FiChevronDown } from "react-icons/fi";
-import { useLocalState } from "@/lib/hooks";
+import { useLocalState, useClickOutside } from "@/lib/hooks";
 import { currencyList } from "./currencyList";
 import { getCurrencySymbol } from "@/lib/features/appSettingsSlice";
 
+const variants = {
+  open: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.3,
+      ease: "easeInOut",
+    },
+  },
+  closed: {
+    opacity: 0,
+    y: "-100%",
+    transition: {
+      duration: 0.3,
+      ease: "easeInOut",
+    },
+  },
+};
+
 const CurrencySelector = () => {
   const [show, setShow] = useState(false);
-  const [currency, setCurrency] = useLocalState("currency", "USD");
+  const [currency, setCurrency] = useLocalState("currency", "usd");
+  const ref = useRef(null);
 
-  const onDisplay = () => {
-    setShow(!show);
+  const handleDropdownDisplay = () => {
+    setShow(show === true ? false : true);
   };
 
-  const onCurrencyChange = (value: any) => {
+  const closeDropdown = () => {
+    setShow(false);
+  };
+
+  const onCurrencyChange = (value: string) => {
     setCurrency(value);
   };
+
+  useClickOutside(ref, closeDropdown);
 
   const filteredCurrencyList = currencyList.filter(
     (item) => item.name !== currency
   );
 
   return (
-    <div className="flex-col relative w-[108px] gap-2" onClick={onDisplay}>
+    <div
+      ref={ref}
+      className="flex-col relative w-[108px] gap-2"
+      onClick={handleDropdownDisplay}
+    >
       {currencyList && (
         <div
           className={`flex items-center gap-2 border-1 border-white/[.05] ${
@@ -30,7 +61,7 @@ const CurrencySelector = () => {
           } px-4 py-2 bg-light-lightBg dark:bg-dark-191`}
         >
           <span className="bg-light-secondaryTextColor dark:bg-dark-text w-6 h-6 m-0 flex items-center justify-center p-1 rounded-full">
-            <span className="mx-auto fill-light-lightBg dark:fill-dark-textDark m-0">
+            <span className="mx-auto text-light-lightBg dark:text-dark-textDark m-0">
               {getCurrencySymbol(currency)}
             </span>
           </span>
@@ -40,17 +71,21 @@ const CurrencySelector = () => {
           <FiChevronDown />
         </div>
       )}
-      {show && (
-        <div className="absolute top-[40px] w-[108px]">
-          {filteredCurrencyList.map((el) => {
+      <motion.ul
+        animate={show ? "open" : "closed"}
+        variants={variants}
+        className="absolute top-[40px] w-[108px]"
+      >
+        {show &&
+          filteredCurrencyList.map((el) => {
             return (
-              <div
+              <li
                 className="flex items-center m-0 gap-2 px-4 py-2 bg-light-lightBg dark:bg-dark-191 last:rounded-b-xl"
                 key={el.id}
                 onClick={() => onCurrencyChange(el.name)}
               >
                 <span className="bg-light-secondaryTextColor dark:bg-dark-text w-6 h-6 flex items-center justify-center p-1 rounded-full">
-                  <span className="mx-auto fill-light-lightBg dark:fill-dark-textDark">
+                  <span className="mx-auto text-light-lightBg dark:text-dark-textDark">
                     {el.symbol}
                   </span>
                 </span>
@@ -58,11 +93,10 @@ const CurrencySelector = () => {
                   {el.name}
                 </span>
                 <FiChevronDown />
-              </div>
+              </li>
             );
           })}
-        </div>
-      )}
+      </motion.ul>
     </div>
   );
 };
