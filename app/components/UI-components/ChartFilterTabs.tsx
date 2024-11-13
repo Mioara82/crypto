9eb751b4f5a361779bb1d8db1b00fd89dedbb583
Mioara82 <128102876+Mioara82@users.noreply.github.com>
2024-11-13 02:20:15 +0000

    this component does not read state from props, it reads the selectedFilter from converterSlice and dispatches the handleSelectedFilter

--- app/components/UI-components/ChartFilterTabs.tsx
+++ app/components/UI-components/ChartFilterTabs.tsx
@@ -1,27 +1,25 @@
 import React from "react";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks/hooks";
 import { chartFilter } from "@/app/utils/chartFilter";
-import { daysObject } from "@/lib/types/types";
+import { handleSelectedFilter } from "@/lib/features/converterSlice";
+import { RootState } from "@/lib/store";
 
-interface ChartFilterTabsProps {
-  days: daysObject;
-  handleSelectedFilter: any;
-}
-
-const ChartFilterTabs: React.FC<ChartFilterTabsProps> = ({
-  days,
-  handleSelectedFilter,
-}) => {
-
+const ChartFilterTabs = () => {
+const selectedFilter = useAppSelector((state:RootState)=>state.converter.selectedFilter);
+const dispatch = useAppDispatch();
+const handleFilter = (id:number) => {
+  dispatch(handleSelectedFilter({id}));
+};
   return (
     <div className="w-[463px] h-[42px] bg-light-lightBg dark:bg-dark-hover rounded-md ">
       <ul className="flex justify-around text-center gap-2">
         {chartFilter.map((title) => (
           <li
             className={`text-sm text-light-secondaryTextColor dark:text-[#a7a7cc] text-center cursor-pointer px-5 py-2 rounded-md ${
-              days && title.id === days.id ? "dark:bg-[#6161d650]" : ""
+              selectedFilter.period && title.id === selectedFilter.id ? "dark:bg-[#6161d650]" : ""
             }`}
             key={title.id}
-            onClick={() => handleSelectedFilter(title.id)}
+            onClick={() => handleFilter(title.id)}
           >
             {title.name}
           </li>
