9b54aaf02a69ada3fb571cb104fdfc0cf00b03c7
Mioara82 <128102876+Mioara82@users.noreply.github.com>
2024-11-13 02:18:46 +0000

    this component now holds its own state to manage the active buttons

--- app/components/UI-components/ButtonGroup.tsx
+++ app/components/UI-components/ButtonGroup.tsx
@@ -1,27 +1,31 @@
 import React from "react";
+import Link from "next/link";
 import Button from "./Button";
+import { useIsActive } from "@/lib/hooks/useIsActive";
 
-const ButtonGroup = React.memo(function ButtonGroup({
-  isActive,
-  handleActiveButton,
-}: {
-  isActive: number | null;
-  handleActiveButton: any;
-}) {
+const ButtonGroup = React.memo(function ButtonGroup() {
+  const [isActive, setIsActive] = useIsActive(0);
+  const handleActiveButton = (value: number) => {
+    setIsActive((prev) => (prev === value ? null : value));
+  };
   return (
     <div className="flex justify-start w-[506px] h-[53px] rounded-md p-1">
-      <Button
-        text="Coins"
-        isActive={isActive === 0}
-        onButtonClick={() => handleActiveButton(0)}
-        feature="nav"
-      />
-      <Button
-        text="Converter"
-        isActive={isActive === 1}
-        onButtonClick={() => handleActiveButton(1)}
-        feature="nav"
-      />
+      <Link href="/" passHref>
+        <Button
+          text="Coins"
+          isActive={isActive === 0}
+          onButtonClick={() => handleActiveButton(0)}
+          feature="nav"
+        />
+      </Link>
+      <Link href="/Converter" passHref>
+        <Button
+          text="Converter"
+          isActive={isActive === 1}
+          onButtonClick={() => handleActiveButton(1)}
+          feature="nav"
+        />
+      </Link>
     </div>
   );
 });
