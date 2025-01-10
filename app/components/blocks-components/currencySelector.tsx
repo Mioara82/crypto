a07fbbfd791bae4a98f5d0639df5ae736005cc5e
Mioara82 <128102876+Mioara82@users.noreply.github.com>
2025-01-10 02:38:56 +0000

    used the Dropdown component

--- app/components/blocks-components/currencySelector.tsx
+++ app/components/blocks-components/currencySelector.tsx
@@ -1,6 +1,5 @@
 "use-client";
 import { useState, useRef } from "react";
-import { motion } from "framer-motion";
 import { useAppSelector, useAppDispatch } from "@/lib/hooks/hooks";
 import { RootState } from "@/lib/store";
 import { useClickOutside } from "@/lib/hooks/useClickOutside";
@@ -8,25 +7,7 @@ import { currencyList } from "../UI-components/currencyList";
 import { setCurrency, Currency } from "@/lib/features/currencySlice";
 import CurrencyDetails from "../UI-components/CurrencyDetails";
 import FilteredCurrencyList from "../UI-components/FilteredCurrencyList";
-
-const variants = {
-  open: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 0.3,
-      ease: "easeInOut",
-    },
-  },
-  closed: {
-    opacity: 0,
-    y: "-100%",
-    transition: {
-      duration: 0.3,
-      ease: "easeInOut",
-    },
-  },
-};
+import Dropdown from "../UI-components/Dropdown";
 
 const CurrencySelector = () => {
   const [show, setShow] = useState(false);
@@ -60,7 +41,6 @@ const CurrencySelector = () => {
 
   return (
     <div
-      ref={ref}
       className="flex-col relative w-[108px] gap-2 z-40"
       onClick={handleDropdownDisplay}
     >
@@ -72,18 +52,14 @@ const CurrencySelector = () => {
         />
  
       )}
-      <motion.ul
-        animate={show ? "open" : "closed"}
-        variants={variants}
-        className="absolute top-[40px] w-[108px]"
-      >
+      <Dropdown show={show} ref={ref} feature="currency list">
         {show && (
           <FilteredCurrencyList
             list={filteredCurrencyList}
             onCurrencyChange={handleCurrencyChange}
           />
         )}
-      </motion.ul>
+      </Dropdown>
     </div>
   );
 };
