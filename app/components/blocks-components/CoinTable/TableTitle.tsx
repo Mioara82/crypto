27f15a121d6a8776a5ecbc8b97a47d8f6925de03
Mioara82 <128102876+Mioara82@users.noreply.github.com>
2025-01-10 02:46:13 +0000

    completely refactored the way the sorting queries are rendered, replaced the select input with a dropdown display,I chose this from UI perspective

--- app/components/blocks-components/CoinTable/TableTitle.tsx
+++ app/components/blocks-components/CoinTable/TableTitle.tsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { useIsShown } from "@/lib/hooks/useIsShown";
+import Dropdown from "../../UI-components/Dropdown";
 import { capitaliseString } from "@/app/utils/formatHelpers";
 import { queriesAPI } from "./CoinsTable";
 
-export const TableTitle = ({value, handleSortChange}:{value: string, handleSortChange:any}) => {
+export const TableTitle = ({
+  value,
+  handleSortChange,
+}: {
+  value: string;
+  handleSortChange: any;
+}) => {
+  const [show, handleIsShown] = useIsShown();
+  const [selectedQuery, setSelectedQuery] = useState<string>(value);
+  const listRef = useRef(null);
+  const filteredQueries = queriesAPI.filter((q:string) => q !== value);
+  const handleQueryChange = (v: string) => {
+    setSelectedQuery(v);
+    handleSortChange(v);
+    handleIsShown();
+  };
+
   return (
-    <div className="text-2xl flex items-center gap-4">
+    <div className="relative flex items-center gap-4 text-2xl">
       TOP 50 <span className="text-lg">BY </span>
-      <div className="relative w-56">
-      <select className="w-full bg-light-primary dark:bg-dark-primaryBg rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" value={value} onChange={handleSortChange}>
-        {queriesAPI.map((query) => (
-          <option key={query} value={query}>
-            {capitaliseString(query)}
-          </option>
-        ))}
-      </select>
-      </div>
+      <div className="hover:cursor-pointer" onClick={handleIsShown}>{capitaliseString(selectedQuery)}</div>
+      <Dropdown ref={listRef} show={show} feature="table dropdown">
+        {show &&
+          filteredQueries.map((q: string) => (
+            <li key={q} onClick={()=> handleQueryChange(q)} className="text-base hover:text-common-cyan hover:cursor-pointer">{capitaliseString(q)}</li>
+          ))}
+      </Dropdown>
     </div>
   );
 };
