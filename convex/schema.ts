41321e444fbf1fc677bca964d63faf590bf7e6b9
Mioara82 <128102876+Mioara82@users.noreply.github.com>
2025-06-15 21:17:29 +0100

    refactored convex schema and functions to save the portfolio coins per specific user

--- convex/schema.ts
+++ convex/schema.ts
@@ -9,13 +9,13 @@ export default defineSchema({
     tokenIdentifier: v.string(),
   }).index("by_token", ["tokenIdentifier"]),
   portfolioCoins: defineTable({
-    userId: v.string(),
-    name:v.string(),
+    userId: v.id("users"), 
+    name: v.string(),
     coinId: v.string(),
     symbol: v.string(),
     image: v.string(),
     currentPrice: v.number(),
     purchaseAmount: v.number(),
     purchasedDate: v.string(),
-  }),
+  }).index("by_user", ["userId"]),
 });
