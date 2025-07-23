6c6e37363ac6c0fdb7e6103208e7ff7988a0e557
Mioara82 <128102876+Mioara82@users.noreply.github.com>
2025-07-23 19:31:05 +0100

    refactor the design of the loading spinners

--- app/components/UI-components/Skeleton/Loading.tsx
+++ app/components/UI-components/Skeleton/Loading.tsx
@@ -1,23 +1,10 @@
-import React, { useState } from "react";
-import ProgressBar from "../progressBar";
-import { useInterval } from "@/lib/hooks/useInterval";
+import React from "react";
+import "@/public/loader-squares.css";
 
 const LoadingCard = () => {
-  const [progress, setProgress] = useState<number>(0);
-  useInterval(() => {
-    setProgress((prev: any) => {
-      if (prev < 100) {
-        return prev + 1;
-      }
-      return 100;
-    });
-  }, 50);
   return (
-    <div className="w-full mt-11 p-6">
-      <ProgressBar value={progress} gradient={true} colorTwo="#5a5a89" />
-      <h3 className="text-sm md:text-base xl:text-xl text-center mt-11">
-        Loading, please wait
-      </h3>
+    <div className="flex min-h-screen w-full flex-col items-center justify-center p-6">
+      <div className="loader-squares"></div>
     </div>
   );
 };
