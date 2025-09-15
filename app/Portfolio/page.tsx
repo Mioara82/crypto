0904cfd6b60bbc9c5dce1364039b53269fcf90d6
Mioara82 <128102876+Mioara82@users.noreply.github.com>
2025-09-15 08:09:01 +0100

    design(modal): fixed alignment and background colour of delete coin modal

--- app/Portfolio/page.tsx
+++ app/Portfolio/page.tsx
@@ -6,7 +6,7 @@ import { useStoreUserEffect } from "@/lib/hooks/useStoreUserEffects";
 import AddAssetModal from "../components/blocks-components/Portfolio/AddAssetModal";
 import Button from "../components/UI-components/Button";
 import Spinner from "../components/UI-components/Spinner";
-import InvestmentCalculator from "../components/blocks-components/Portfolio/InvestmentCalculator";
+//import InvestmentCalculator from "../components/blocks-components/Portfolio/InvestmentCalculator";
 import type { Doc } from "@/convex/_generated/dataModel";
 
 const AssetCoins = lazy(
@@ -19,7 +19,7 @@ const Portfolio = () => {
   const [show, handleIsShown] = useIsShown();
   const [mode, setMode] = useState<"add" | "edit">("add");
   const [editingCoin, setIsEditingCoin] = useState<PortfolioCoin | null>(null);
-  const [isCalculatorOpen, setIsCalculatorOpen] = useState<boolean>(false);
+  //const [isCalculatorOpen, setIsCalculatorOpen] = useState<boolean>(false);
 
   const openAddForm = () => {
     setMode("add");
@@ -33,9 +33,9 @@ const Portfolio = () => {
     handleIsShown();
   };
 
-  const handleCalculatorDisplay = () => {
-    setIsCalculatorOpen((prev) => !prev);
-  };
+  // const handleCalculatorDisplay = () => {
+  //   setIsCalculatorOpen((prev) => !prev);
+  // };
 
   const { isLoading, isAuthenticated, userId } = useStoreUserEffect();
 
@@ -59,17 +59,17 @@ const Portfolio = () => {
     <div className="relative">
       <main
         className={`relative flex min-h-screen flex-col p-4 sm:p-8 md:p-24 ${
-          show || isCalculatorOpen ? "z-0 blur-sm" : "blur-none"
+          show ? "z-0 blur-sm" : "blur-none"
         }`}
       >
         <p className="mb-6 text-sm lg:text-base">Your statistics</p>
         <div className="mb-6 flex flex-col items-center justify-between sm:flex-row">
           <div className="m-auto flex items-center justify-around gap-3">
-            <Button
+            {/* <Button
               text="Investments Calculator"
               feature="large"
               onButtonClick={handleCalculatorDisplay}
-            />
+            /> */}
             <Button
               text="Add asset"
               feature="large"
@@ -90,11 +90,11 @@ const Portfolio = () => {
           />
         </div>
       )}
-      {isCalculatorOpen && (
+      {/* {isCalculatorOpen && (
         <InvestmentCalculator
           handleCalculatorDisplay={handleCalculatorDisplay}
         />
-      )}
+      )} */}
     </div>
   );
 };
